6d92ecd47050890e2b1d38686c78d470568a2776
feanor77ist <a.ardas@gmail.com>
2024-02-28 20:01:28 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css'; // Ana Stil dosyası yolu
 import videoSource from "./images/short.mp4"
 import Navigation from './Navigation';
@@ -6,6 +6,7 @@ import './Navigation.css'; // NavBar Stil dosyası yolu
 import Video from './Video';
 import Poetry from './Poetry';
 import useVideoControls from './useVideoControls';
+import MyTypewriter from './TypeWriter';
 
 // Uygulamanın ana bileşeni
 function App() {
@@ -14,6 +15,11 @@ function App() {
 
   return (
     <div className="app-container">
+      <div className="typewriter-container">
+        <div className="typewriter-text">
+          <MyTypewriter />
+        </div>
+      </div>
       <Navigation />
       <Video 
         src={videoSource} 
---
+++ src/TypeWriter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+
+const MyTypewriter = () => {
+  return (
+    <Typewriter
+      options={{
+        loop: true,
+        delay: 25,
+      }}
+      onInit={(typewriter) => {
+        typewriter
+            .typeString('ANKARA\'DAYMIŞ <span style="background-color: #a8876e;">BARIŞ</span>')
+            .pauseFor(2000)           
+            .deleteChars(10)
+            .pauseFor(1000)
+            .typeString('<span> </span><span style="background-color: #a8876e;">BARIŞ</span>')
+            .pauseFor(1000)
+            .deleteChars(5)
+            .pauseFor(1000)
+            .typeString('<span> </span><span style="background-color: #a8876e;">MIYDI</span> BARIŞ ?')
+            .pauseFor(2000)
+            .start();
+      }}
+    />
+  );
+};
+
+export default MyTypewriter;
